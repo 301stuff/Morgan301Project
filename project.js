ab0cdcdea9e911ab63c5cf28922df096772f1069
mts513 <34582160+mts513@users.noreply.github.com>
2019-05-22 13:31:31 -0400

    Added images & styling

--- project.js
+++ project.js
@@ -1,12 +1,24 @@
 //selects the divs that contain the cards
 var card = document.getElementsByClassName("cards");
 var cardValue = ["A", "A", "B", "B", "C", "C", "D", "D"];
+var selectedCard = getElementsByClassName("selected");
 
 //event listener for all cards
 for(var i = 0; i < card.length; i++){
     card[i].addEventListener("click", function()
     {
-        alert("hello");
+        this.className = "selected";      
     }
     )};
+//assign values to cards
+for(var i = 0; i < card.length; i++){
+    card[i].innerHTML = cardValue[i];
+};
 
+// first click is selected card
+var selectedCard 
+// randomize assign
+//logic that compares selected cards
+//if match hide or remove cards with a status
+// else cover ID and alert Try Again!
+// update status class for each pair that is matched
